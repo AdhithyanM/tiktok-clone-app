e5184495d3f1627738a1e8595132f2ae46287c7c
AdhithyanM <adhithyanmanoharan26@gmail.com>
2023-03-20 00:54:42 +0530

    handling server side props and setting up components

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,10 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { allPostsQuery } from "../../../utils/queries";
+import { client } from "../../../utils/client";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === "GET") {
     const query = allPostsQuery();
 
-    // const data = await client...
+    const data = await client.fetch(query);
+
+    res.status(200).json(data);
   }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,35 @@
-import type { NextPage } from "next";
 import axios from "axios";
+import { Video } from "../types";
+import VideoCard from "../components/VideoCard";
+import NoResults from "../components/NoResults";
 
-const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+interface IProps {
+  videos: Video[];
+}
+
+// all the props from getServerSideProps will be injected automatically into the Home function by next js.
+const Home = ({ videos }: IProps) => {
+  return (
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+      ) : (
+        <NoResults text={"No Videos"} />
+      )}
+    </div>
+  );
 };
 
 // fetching data
 export const getServerSideProps = async () => {
-  const response = await axios.get(`http://localhost:3000/api/post`);
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
 
-  console.log(response.data.name);
+  console.log(data);
 
   return {
-    props: {},
+    props: {
+      videos: data,
+    },
   };
 };
 
